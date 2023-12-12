4d7687edffc94500c844eb247594d6e69699d1cb
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2023-12-12 22:45:42 +0300

    routing fix

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import restraunt from "../images/restraunt.jpeg";
 import marioadrian from "../images/mario-and-adrian.jpg";
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 import logo from "../images/little-lemon-logo.svg";
 
 
@@ -9,23 +10,23 @@ const Footer = () => {
                 <img className="footer__logo" src={logo}></img>
                 <ul className="footer__nav__list">
                     <li className="footer__nav__heading">Doormat Navigation</li>
-                    <li className="footer__nav__item">Home</li>
-                    <li className="footer__nav__item">About</li>
-                    <li className="footer__nav__item">Menu</li>
-                    <li className="footer__nav__item">Reservations</li>
-                    <li className="footer__nav__item footer__nav__item_highlighted">Order Online</li>
+                    <Link to="/"><li className="footer__nav__item">Home</li></Link>
+                    <Link to="/#about"><li className="footer__nav__item">About</li></Link>
+                    <Link to="/#menu"><li className="footer__nav__item">Menu</li></Link>
+                    <Link to="/booking"><li className="footer__nav__item">Reservations</li></Link>
+                    <Link to="/delivery"><li className="footer__nav__item footer__nav__item_highlighted">Order Online</li></Link>
                 </ul>
                 <ul className="footer__nav__list">
                     <li className="footer__nav__heading">Contact</li>
-                    <li className="footer__nav__item">Address</li>
-                    <li className="footer__nav__item">Telephone</li>
-                    <li className="footer__nav__item">Email</li>
+                    <li className="footer__nav__item">Chicago, 1803 Pinewood Drive</li>
+                    <li className="footer__nav__item">(+1 312) 803-0095</li>
+                    <li className="footer__nav__item">order@lemon.com</li>
                 </ul>
                 <ul className="footer__nav__list">
                     <li className="footer__nav__heading">Social Media Links</li>
-                    <li className="footer__nav__item">Facebook</li>
-                    <li className="footer__nav__item">Instagram</li>
-                    <li className="footer__nav__item">Telegram</li>
+                    <Link to="https://www.facebook.com/" target="_blank"><li className="footer__nav__item">Facebook</li></Link>
+                    <Link to="https://www.instagram.com/" target="_blank"><li className="footer__nav__item">Instagram</li></Link>
+                    <Link to="https://t.me/telegram" target="_blank"><li className="footer__nav__item">Telegram</li></Link>
                 </ul>
             </nav>
         </footer>
